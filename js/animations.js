e79dec41717d02e4e636ac0c7695a2af00f1e687
mely201 <meluore19@gmail.com>
2021-03-02 12:49:36 -0600

    Portafolio web

---
+++ js/animations.js
@@ -0,0 +1,47 @@
+window.sr = ScrollReveal();
+
+sr.reveal('header',{
+ duration:3000,
+ origin: 'bottom',
+ distance:'-100px'
+});
+sr.reveal('.scroll-banner',{
+    duration:3000,
+    origin: 'bottom',
+    distance:'-100px'
+   });
+   sr.reveal('.scroll-heading',{
+    duration:3000,
+    origin: 'top',
+    distance:'80px'
+   });
+   sr.reveal('.scroll-contentBx',{
+    duration:3000,
+    origin: 'left',
+    distance:'80px'
+   });
+   sr.reveal('.scroll-img',{
+    duration:3000,
+    origin: 'right',
+    distance:'80px'
+   });
+   sr.reveal('.scroll-services',{
+    duration:3000,
+    origin: 'top',
+    distance:'100px'
+   });
+   sr.reveal('.scroll-work',{
+    duration:3000,
+    origin: 'top',
+    distance:'100px'
+   });
+   sr.reveal('.scroll-testimonial',{
+    duration:3000,
+    origin: 'right',
+    distance:'100px'
+   });
+   sr.reveal('.scroll-testimonial',{
+    duration:3000,
+    origin: 'right',
+    distance:'100px'
+   });
